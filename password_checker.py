bba9ae86676210be00673c990c0a9549f56eb080
zhang <zhangmingxuan162@gmail.com>
2022-03-31 11:48:39 +0800

    Add docstrings to password checker

--- password_checker.py
+++ password_checker.py
@@ -1,5 +1,11 @@
+"""
+Description: A simple password checker
+Name: Mingxuan Zhang
+"""
+
 MINIMUM_LENGTH = 8
 def main():
+    """start program"""
     password = input("Please enter a password at least {} characters: ".format(MINIMUM_LENGTH))
     while len(password) < 8:
         print("Password is too short!")
